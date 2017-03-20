7af3bc19cb7cf9b6d7bf24d6217b9d46c780be6f
Chul Ahn <scahn.x@gmail.com>
2017-03-20 14:08:38 -0400

    more routing

--- app/app.module.ts
+++ app/app.module.ts
@@ -6,7 +6,9 @@ import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './home/welcome.component';
+
 import { ProductListComponent } from './products/product-list.component';
+import { ProductDetailGuard } from './products/product-guard.service';
 import { ProductDetailComponent } from './products/product-detail.component';
 import { ProductFilterPipe } from './products/product-filter.pipe';
 import { StarComponent } from './shared/star.component';
@@ -18,7 +20,10 @@ import { StarComponent } from './shared/star.component';
         HttpModule,
         RouterModule.forRoot([
             { path: 'products', component: ProductListComponent },
-            { path: 'products/:id', component: ProductDetailComponent },
+            { path: 'products/:id', 
+                canActivate: [ ProductDetailGuard],
+                component: ProductDetailComponent
+            },
             { path: 'welcome', component: WelcomeComponent },
             { path: '', redirectTo: 'welcome', pathMatch: 'full' },
             { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
@@ -32,6 +37,7 @@ import { StarComponent } from './shared/star.component';
       ProductFilterPipe,
         StarComponent
     ],
+  providers: [ ProductDetailGuard ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
